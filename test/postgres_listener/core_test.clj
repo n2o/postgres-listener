12ff8a1693882f6cd283ad7368a5692abc96bae7
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-24 23:04:05 +0200

    Rename test namespace

---
+++ test/postgres_listener/core_test.clj
@@ -0,0 +1,7 @@
+(ns postgres-listener.core-test
+  (:require [clojure.test :refer :all]
+            [postgres-listener.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is true)))
