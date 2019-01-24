d54bcac5fefa11d2cd364de2dc775987f2d3b714
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-01-24 22:27:00 +0100

    Update deps and version

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject de.hhu.cn/postgres-listener "0.1.2"
+(defproject de.hhu.cn/postgres-listener "0.1.3"
   :description "Connect to a postgres-database and wait for a NOTIFY event to trigger a function"
   :url "https://github.com/n2o/postgres-listener"
   :license {:name "MIT"
             :url "https://choosealicense.com/licenses/mit/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.7.1"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.taoensso/timbre "4.10.0"]  ;; logging lib
-                 ]
+                 [com.taoensso/timbre "4.10.0"]]
   :main ^:skip-aot postgres-listener.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
